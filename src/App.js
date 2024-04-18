2a5966157cdd95ea9839bab9251ea90a366b96e4
Abhay Kumar Singh <abhayanuj22@gmail.com>
2024-04-18 15:55:39 +0530

    Created App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Form from './pages/Form.js';
+import DisplayData from './pages/DisplayData.js';
+import Home from './pages/Home.js'
+import Error from './pages/Error.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/form' element={<Form/>}/>
+          <Route path='/:id' element={<DisplayData/>}/>
+          <Route path='*' element={<Error/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
